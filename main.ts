967b2472bb7c09ed853442bcec43ec57aec6b871
Akshat <47287597+shatak@users.noreply.github.com>
2024-04-25 12:54:11 -0400

    layer fixes, removed extra assets

--- main.ts
+++ main.ts
@@ -1219,26 +1219,26 @@ let tilemap1: tiles.TileMapData = null
 tilemap1 = tilemap`level0`
 let tilemap2 = tilemap`level`
 tileUtil.connectMaps(tilemap1, tilemap2, MapConnectionKind.Door1)
-setLevel()
 playerSprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
-    . . . . f f f e e f f f . . . . 
-    . . . f e e e e e e e f f . . . 
-    . . f e e e e e e e e e f f . . 
-    . . f e e e e e e e e e e f . . 
-    . . f e e e e e e e e e e f . . 
-    . . f e e f e e e e f e e f . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
     . f f e f b f 4 4 f b f e f f . 
-    . f 2 e 4 1 f d d f 1 4 e 2 f . 
-    . . f 2 e d d d d d d e 2 f . . 
-    . . . f 2 2 4 4 4 4 2 2 f . . . 
-    . . e a 9 9 f f f f f 9 a e . . 
-    . . 4 d a 9 9 9 f 9 9 9 d 4 . . 
-    . . 4 4 f a a 9 9 a a f 4 4 . . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
 scene.cameraFollowSprite(playerSprite)
+setLevel()
 tiles.placeOnTile(playerSprite, tiles.getTileLocation(0, 0))
 setMovement(100)
 pokedex = [
